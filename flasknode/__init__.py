023a321c50758aadf55ba1d408ede67eb04e4411
Michael Roberts <michael@vivtek.com>
2019-11-14 17:09:15 -0400

    added CORS to Flask to allow Vue hot dev mode

--- flasknode/__init__.py
+++ flasknode/__init__.py
@@ -1,6 +1,8 @@
 from flask import Flask
+from flask_cors import CORS
 app = Flask(__name__,
             static_folder = "static")
+CORS(app)
 
 from flasknode import views
 from flasknode import static_files
