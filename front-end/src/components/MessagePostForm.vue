5a049e869fff11246fece229dc6532ae400711b2
Michael Roberts <michael@vivtek.com>
2019-11-15 16:33:00 -0400

    added MessagePostForm in front end

---
+++ front-end/src/components/MessagePostForm.vue
@@ -0,0 +1,40 @@
+<template>
+ <v-card>
+   <v-form
+     ref="form"
+   >
+     <v-text-field
+      v-model="message"
+     ></v-text-field>
+     <v-btn
+      color="success"
+      class="mr-4"
+      @click="post"
+     >
+      POST
+     </v-btn>
+   </v-form>
+ </v-card>
+</template>
+
+<script>
+import axios from 'axios'
+
+export default {
+  name: 'MessagePostForm',
+
+  data: () => ({
+    message: null
+  }),
+  methods: {
+    post () {
+       axios
+          .post('http://localhost:5000/message/post',
+             {
+                message: this.message,
+             }
+          )
+    }
+  }
+};
+</script>
