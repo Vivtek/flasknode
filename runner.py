54545d7456fff91f67d2e8b7e58a456c03b84563
Michael Roberts <michael@vivtek.com>
2019-11-17 18:26:33 -0400

    Removed commented-out socketio invocation

--- runner.py
+++ runner.py
@@ -2,5 +2,4 @@ from flasknode import app
 from flask_socketio import SocketIO
 from flasknode import socketio
 
-#socketio = SocketIO(app, cors_allowed_origins="*")
 socketio.run(app, log_output=True)
