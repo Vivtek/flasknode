a81e3f8845558102eaa6dbea5b740735b6302257
Michael Roberts <michael@vivtek.com>
2019-11-17 21:29:32 -0400

    Cleanup of failed attempt to get socketio defined

--- flasknode/__init__.py
+++ flasknode/__init__.py
@@ -5,7 +5,7 @@ app = Flask(__name__,
             static_folder = "static")
 CORS(app)
 
-socketio = SocketIO(app, cors_allowed_origins="*")
+socketio = SocketIO(app, cors_allowed_origins=['http://localhost:8080','http://127.0.0.1:8080'])
 # Note on cors_allowed_origins: https://stackoverflow.com/questions/29187933/flask-socketio-cors
 
 
