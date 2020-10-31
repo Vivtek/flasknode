2eef437856397fc51de3405a7af74f801995b19e
Michael Roberts <michael@vivtek.com>
2020-10-30 23:23:37 -0400

    Check our external IP against internal

--- flasknode/__init__.py
+++ flasknode/__init__.py
@@ -4,6 +4,7 @@ from flasknode import subnet
 from flask import Flask
 from flask_socketio import SocketIO
 from flask_cors import CORS
+import requests
 
 config = configparser.ConfigParser()
 configfile = os.path.normpath (os.path.join(os.path.abspath(os.path.dirname(sys.argv[0])), 'setup.conf'))
@@ -11,6 +12,8 @@ config.read(configfile)
 
 my_ip = subnet.get_ip()
 print ("Starting on IP: %s" % (my_ip))
+external_ip = requests.get('https://api.ipify.org').text
+print ("IP to the outside: %s" % external_ip)
 
 app = Flask(__name__,
             static_folder = "static")
@@ -19,6 +22,11 @@ CORS(app)
 app.my_ip = my_ip
 app.fnconfig = config
 app.this_node = '_unknown_node_'
+if my_ip == external_ip:
+  app.connectable = True
+else:
+  app.connectable = False
+  print ("Warning: node is not connectable")
 
 subnet.start_subnet_maintenance(app)
 
